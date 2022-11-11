e447c99c5ac2f8de59a8133b543c3add0e2bfb02
thiagobarretoo <thiagoysb@gmail.com>
2022-11-10 22:11:45 -0300

    formulario

---
+++ script.js
@@ -0,0 +1,7 @@
+ function salva() {
+
+     var nome = document.getElementById("input-name");
+     if (inputname.value != "") {
+         alert('Obrigado sr(a)' + inputname.value + ' seu cadastro foi realizado com sucesso')
+     }
+ }
